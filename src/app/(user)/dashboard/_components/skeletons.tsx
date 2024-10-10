9e0808643cd079f14e40f30fb2cfb9b08e75686e
Khalil <khalilpati55@gmail.com>
2024-10-10 15:05:21 +0530

    Fix dashboard loading UI

--- src/app/(user)/dashboard/_components/skeletons.tsx
+++ src/app/(user)/dashboard/_components/skeletons.tsx
@@ -29,7 +29,7 @@ export const ProjectCardSkelton = () => {
 
 export const ProjectListSkeleton = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
       <ProjectCardSkelton />
       <ProjectCardSkelton />
       <ProjectCardSkelton />
