6e74845baaa53ef9a9e9a7acce743f78eff59247
Khalil <khalilpati55@gmail.com>
2024-10-12 22:08:29 +0530

    Built pricing section

---
+++ src/app/(home)/_components/pricing-card.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Plan } from "./pricing-section";
+import { Check } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+const PricingCard = ({ plan }: { plan: Plan }) => {
+  return (
+    <div className="tarnsition relative flex h-full flex-col justify-between rounded border bg-white/5 p-6 backdrop-blur-sm hover:shadow">
+      {plan.isPopular && (
+        <div className="absolute right-0 top-0 rounded-bl-lg rounded-tr-lg bg-primary px-2 py-1 text-sm text-primary-foreground">
+          Popular
+        </div>
+      )}
+      <div>
+        <div className="items-end">
+          <h4 className="text-3xl font-extrabold">${plan.price}</h4>
+          <p className="my-2 text-xl font-bold">{plan.title}</p>
+        </div>
+        <p className="text-balance">{plan.description}</p>
+      </div>
+      <div>
+        <div className="my-4 flex-grow border-t border-gray-400 opacity-25" />
+        <ul>
+          {plan.features.map((feature) => (
+            <li
+              key={feature}
+              className="my-2 flex flex-row items-center gap-2 text-muted-foreground">
+              <div className="mr-2 rounded-full bg-primary p-1 text-primary-foreground">
+                <Check size={14} className="text-primary-foreground" />
+              </div>
+              {feature}
+            </li>
+          ))}
+        </ul>
+        <Button className="mt-4 w-full">Get Started</Button>
+      </div>
+    </div>
+  );
+};
+
+export default PricingCard;
---
+++ src/app/(home)/_components/pricing-section.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import PricingCard from "./pricing-card";
+
+export type Plan = {
+  title: string;
+  price: number;
+  description: string;
+  features: string[];
+  isPopular?: boolean;
+};
+
+export const plans: Plan[] = [
+  {
+    title: "Free",
+    price: 0,
+    description: "For small teams or individuals",
+    features: [
+      "3 projects",
+      "Unlimited users",
+      "2GB storage",
+      "Priority support",
+    ],
+  },
+  {
+    title: "Monthly",
+    price: 6.99,
+    description: "For growing teams",
+    features: [
+      "Unlimited projects",
+      "Unlimited users",
+      "5GB storage",
+      "Priority support",
+    ],
+    isPopular: true,
+  },
+  {
+    title: "Yearly",
+    price: 39.99,
+    description: "Upgrade to save more!",
+    features: [
+      "Unlimited users",
+      "Unlimited projects",
+      "50GB storage",
+      "24/7 support",
+    ],
+  },
+];
+
+const PricingSection = () => {
+  return (
+    <section>
+      <h2 className="text-center text-3xl capitalize">Pricing</h2>
+      <p className="mb-8 mt-3 text-center text-2xl font-extrabold">
+        Flexible Pricing to fit your needs.
+      </p>
+      <ul className="grid max-w-screen-xl grid-cols-1 items-center gap-3 md:grid-cols-3">
+        {plans.map((plan) => (
+          <li key={plan.title} className="h-full">
+            <PricingCard plan={plan} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default PricingSection;
