438c29540db65b0c163aebd9345ad05d4fbca8ac
Khalil <khalilpati55@gmail.com>
2024-10-12 22:08:12 +0530

    Moved the homepage to another route group

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PricingSection from "./_components/pricing-section";
+
+const LandingPage = () => {
+  return (
+    <div className="flex flex-1 flex-col py-5">
+      <PricingSection />
+    </div>
+  );
+};
+
+export default LandingPage;
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex h-screen flex-col items-center justify-center gap-4">
-      <h1 className="text-3xl font-bold">Welcome to Next.js!</h1>
-    </div>
-  );
-}
