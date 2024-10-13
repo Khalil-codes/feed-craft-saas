13495af2d03d72349651b5b98950a5955666928a
Khalil <khalilpati55@gmail.com>
2024-10-13 15:36:01 +0530

    Smooth scrolling and redirect changes in clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      signInFallbackRedirectUrl={"/dashboard"}
+      signUpFallbackRedirectUrl={"/"}>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
